dac51ff1a8c54510ca1403c2769556453f6a31c4
Aldair Cuarez <aldaircuarez98@gmail.com>
2024-04-24 00:20:51 -0500

    Update ApiController.cs

--- Api/Controllers/ApiController.cs
+++ Api/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Api.Controllers;
 
@@ -8,15 +9,29 @@ namespace Api.Controllers;
 public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors){
+        // if (errors.All(e => e.Type == ErrorType.Validation))
+        // {
+        //     var modelStateDictionary = new ModelStateDictionary();
+        //     foreach (var error in errors) {
+        //         modelStateDictionary.AddModelError(error.Code, error.Description);
+        //     }
+        //     return ValidationProblem(modelStateDictionary);
+        // }
+
+        // if (errors.Any(e => e.Type == ErrorType.Unexpected)){
+        //     return Problem();
+        // }
+
         var firstError = errors[0];
         var statusCode = firstError.Type switch
         {
-            ErrorType.NotFound => StatusCodes.Status404NotFound,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             _ => StatusCodes.Status500InternalServerError
 
         };
-        return Problem(firstError.Description.ToString(), statusCode.ToString());
+        return Problem(statusCode: statusCode, title: firstError.Description);
     }
 }
